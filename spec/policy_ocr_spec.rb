830e0d78eadbcdc67ebc810b391556f8c55eb2ac
Val <vbenetti46@gmail.com>
2021-07-25 12:47:40 -0400

    Update policy_ocr_spec.rb

--- spec/policy_ocr_spec.rb
+++ spec/policy_ocr_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/policy_ocr'
+require 'spec_helper'
 require 'tempfile'
 
 
@@ -12,76 +13,57 @@ describe PolicyOcr do
   it 'loads the sample.txt' do
     expect(fixture('sample').lines.count).to eq(44)
   end
+
+  # Have it read the file and equate to the given information
+  # it 'reads file and equate to the numbers' do
+    
+  # end
 end
 
-# Creating new PolicyOcr Test
-# describe PolicyOcr do
-#   # Moved subject out and make it, it's own constant for eatch context
-#   subject {
-#     Tempfile.open('numbers.txt') do |f|
-#       populate_files.(f)
-#       f.close
-#       PolicyOcr.read_file(f.path, &account_numbers)
-#     end
-#   }
 
-#   context 'read an empty file' do
-#     let(:populate_files) {->(file){}}
+# Creating a different PolicyOcr Test to do the simple nil test 
+describe PolicyOcr do
+  # Moved subject out and make it, it's own constant for eatch context
+  subject {
+    Tempfile.open('numbers.txt') do |f|
+      populate_files.(f)
+      f.close
+      PolicyOcr.read_file(f.path, &account_numbers)
+    end
+  }
 
-#     context 'shoudl call the account number' do
-#       let(:account_numbers) { -> (acct_num_or_nil) {raise 'Has to be called'} }
-#       it 'throws error' do
-#         expect{subject}.to raise_error
-#       end
-#     end
+  context 'read an empty file' do
+    let(:populate_files) {->(file){}}
 
-#     context 'should be a nil when it reaches end of file' do
-#       let(:account_numbers) { -> (acct_num_or_nil) {acct_num_or_nil.should be_nil}}
-#       it {subject}
-#     end
-#   end
+    context 'shoudl call the account number' do
+      let(:account_numbers) { -> (acct_num_or_nil) {raise 'Has to be called'} }
+      it 'throws error' do
+        expect{subject}.to raise_error
+      end
+    end
 
-#   context 'have it read and empty file' do
-#     let(:space) { ' ' }
-#     let(:line_end) {"\n"}
-#     let(:line) {space + line_end }
-#     let(:accumulator) { [] }
-#     let(:account_numbers) { -> (lines) { accumulator << lines } }
-#     let(:populate_files) { ->(file) { file.write line * 8 } }
+    context 'should be a nil when it reaches end of file' do
+      let(:account_numbers) { -> (acct_num_or_nil) {acct_num_or_nil.should be_nil}}
+      it {subject}
+    end
+  end
 
-#     before { subject }
+  context 'have it read and empty file' do
+    let(:space) { ' ' }
+    let(:line_end) {"\n"}
+    let(:line) {space + line_end }
+    let(:accumulator) { [] }
+    let(:account_numbers) { -> (lines) { accumulator << lines } }
+    let(:populate_files) { ->(file) { file.write line * 8 } }
 
-#     it 'passes lines in groups of 3 to the account number parser, leaving off the blank line' do
-#       accumulator.should == [[space, space, space], [space, space, space], nil]
-#     end
-#   end
-# end
+    before { subject }
 
-
-# describe PolicyOcr do
-#   subject {
-#     File.open('/fixtures/sample.txt') do |f|
-#       populate_files.(f)
-#       f.close
-#       PolicyOcr.read_file(f.path, &account_numbers)
-#     end
-#   }
-
-#   context 'can read the file' do
-    
-#   end
-# end
+    it 'should pass the lines in groups of 3 into the number converter, removing blank lines' do
+      accumulator.should == [[space, space, space], [space, space, space], nil]
+    end
+  end
+end
 
 
-# Having trouble trying to get it to read the file
-# Need to look up how to get rspec to open and read what is in the file
-# describe 'grabbing the sample file and reading the numbers' do
-#   let(:path) {File.open("/fixtures/sample.txt")}
-#   let(:converted_acct_numb) {['000000000', '111111111', '222222222',
-#     '333333333', '444444444', '555555555',
-#     '666666666', '777777777', '888888888',
-#     '999999999', '123456789']}
-#   subject {described_class.get_accounts(path)}
-#   it {should == converted_acct_numb}
-# end
+
 
